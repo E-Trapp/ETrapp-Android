cbfaf0dcd60d545e739859f3fa542dfcb47bd093
tankintat <tankintat@gmail.com>
2017-12-04 19:12:16 +0100

    Change event object

--- app/src/main/java/cat/udl/eps/etrapp/android/models/Event.java
+++ app/src/main/java/cat/udl/eps/etrapp/android/models/Event.java
@@ -1,77 +1,80 @@
 package cat.udl.eps.etrapp.android.models;
 
-import java.util.Random;
-
 public class Event {
 
     private long id;
     private long owner;
     private String title;
-    private long created_at;
-    private long updated_at;
+    private long category;
     private String description;
-    private String imageUrl;
-    private long datetime;
+    private long startsAt;
     private boolean isFeatured;
 
-    public Event(long id, String title, String description, long datetime, String imageUrl) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.datetime = datetime;
-        this.created_at = System.currentTimeMillis();
-        this.updated_at = created_at;
-        this.imageUrl = imageUrl;
-        owner = Math.abs(new Random().nextInt() % 3);
-    }
 
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public long getOwner() {
-        return owner;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public long getCreated_at() {
-        return created_at;
-    }
-
-    public long getUpdated_at() {
-        return updated_at;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public long getDatetime() {
-        return datetime;
+    public Event() {
     }
 
     public boolean isFeatured() {
         return isFeatured;
     }
 
-    @Override public String toString() {
+    public long getStartsAt() {
+        return startsAt;
+    }
+
+
+    public void setStartsAt(long startsAt) {
+        this.startsAt = startsAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getOwner() {
+        return owner;
+    }
+
+    public void setOwner(long owner) {
+        this.owner = owner;
+    }
+
+    public long getCategory() {
+        return category;
+    }
+
+    public void setCategory(long category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
         return "Event{" +
                 "id=" + id +
                 ", owner=" + owner +
                 ", title='" + title + '\'' +
-                ", created_at=" + created_at +
-                ", updated_at=" + updated_at +
+                ", category=" + category +
                 ", description='" + description + '\'' +
-                ", imageUrl='" + imageUrl + '\'' +
-                ", datetime=" + datetime +
-                ", isFeatured=" + isFeatured +
                 '}';
     }
 }
