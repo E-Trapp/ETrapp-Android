0f7656cbf144c6301fae67ad58b6e536e710c569
Josep B <ballber@gmail.com>
2018-01-08 12:04:58 +0100

    Offline Mode Message - Error establishing connection to etrapp server (OfflineDialogFragment)

--- app/src/main/java/cat/udl/eps/etrapp/android/ui/fragments/OfflineDialogFragment.java
+++ app/src/main/java/cat/udl/eps/etrapp/android/ui/fragments/OfflineDialogFragment.java
@@ -1,19 +1,45 @@
 package cat.udl.eps.etrapp.android.ui.fragments;
 
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import cat.udl.eps.etrapp.android.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class OfflineDialogFragment extends Fragment {
 
+/**
+ * Created by joe on 8/1/18.
+ */
+
+public class OfflineDialogFragment extends DialogFragment {
+
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_error_connection)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // MODE OFFLINE ALERT
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
 
     public OfflineDialogFragment() {
         // Required empty public constructor
