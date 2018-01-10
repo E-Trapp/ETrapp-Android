a33937bbee8dade213bf1fb8a910adca2b9e0551
tankintat <tankintat@gmail.com>
2018-01-10 19:49:12 +0100

    Home content style changes

--- app/src/main/java/cat/udl/eps/etrapp/android/ui/viewHolders/HomeContentViewHolder.java
+++ app/src/main/java/cat/udl/eps/etrapp/android/ui/viewHolders/HomeContentViewHolder.java
@@ -13,7 +13,7 @@ import cat.udl.eps.etrapp.android.ui.base.ButterizedViewHolder;
 public class HomeContentViewHolder extends ButterizedViewHolder {
 
     @BindView(R.id.home_content_card) public ViewGroup container;
-    @BindView(R.id.home_content_iv) public SimpleDraweeView home_content_image;
+    //@BindView(R.id.home_content_iv) public SimpleDraweeView home_content_image;
     @BindView(R.id.home_content_title) public TextView home_content_title;
     @BindView(R.id.home_content_updated) public TextView home_content_updated;
     @BindView(R.id.home_content_owner) public TextView home_content_owner;
